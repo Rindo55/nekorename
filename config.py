cddf29e9c51e0dabacfc02e24266b2e20819890b
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-17 16:49:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = "6284581524:AAEHd9Idbj2sk5bARTkXX1CS_dGOXOgXuy4"
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = 6
+    API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
     # Update channel for Force Subscribe
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+    UPDATE_CHANNEL = "Neko_Bots"
     # log channel
     #LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
     # Get these values from my.telegram.org
